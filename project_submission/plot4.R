48249a8cc7446c4fa9eadf3781c304f5e47c00f9
Aatish Kumar <aatishk@live.com>
2014-06-08 20:50:01 +0200

    typographic corrections

--- project_submission/plot4.R
+++ project_submission/plot4.R
@@ -2,22 +2,22 @@
 fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 
 # create a temporary directory
-td = tempdir()
+td <- tempdir()
 
 # create the placeholder file
-tf = tempfile(tmpdir=td, fileext=".zip")
+tf <- tempfile(tmpdir=td, fileext=".zip")
 
 # download into the placeholder file (curl method needed for Mac OS X)
 download.file(fileurl, tf, method="curl")
 
 # get the name of the first file in the zip archive
-fname = unzip(tf, list=TRUE)$Name[1]
+fname <- unzip(tf, list=TRUE)$Name[1]
 
 # unzip the file to the temporary directory
 unzip(tf, files=fname, exdir=td, overwrite=TRUE)
 
 # fpath is the full path to the extracted file
-fpath = file.path(td, fname)
+fpath <- file.path(td, fname)
 
 # read the data for February 1-2, 2007 using sql query
 library(sqldf)
@@ -31,7 +31,7 @@ day12 <- within( day12, Date2 <- as.Date(Date, "%d/%m/%Y") )
 day12 <- within( day12, DateTime <- strptime(paste(Date2, Time), format="%Y-%m-%d %H:%M:%S") )
 
 # indicate the missing data labels i.e. ?
-day12 = data.frame(day12, na.strings="?")
+day12 <- data.frame(day12, na.strings="?")
 
 # generate plot4
 png(file="plot4.png")
